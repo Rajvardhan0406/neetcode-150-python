be0108407f69b4915bec91b0e5125a2c24eeafd6
Raj Vardhan Srivastava <vraj35415@gmail.com>
2026-07-29 14:09:16 +0530

    Time: 129 ms (92.41%), Space: 46.4 MB (72.89%) - LeetHub

---
+++ 1146-snapshot-array/1146-snapshot-array.py
@@ -0,0 +1,18 @@
+import bisect
+
+class SnapshotArray:
+    def __init__(self, length: int):
+        self.snap_id = 0
+        self.history = [[(0, 0)] for _ in range(length)]
+
+    def set(self, index: int, val: int) -> None:
+        self.history[index].append((self.snap_id, val))
+
+    def snap(self) -> int:
+        self.snap_id += 1
+        return self.snap_id - 1
+
+    def get(self, index: int, snap_id: int) -> int:
+        records = self.history[index]
+        idx = bisect.bisect_right(records, (snap_id, float('inf'))) - 1
+        return records[idx][1]
