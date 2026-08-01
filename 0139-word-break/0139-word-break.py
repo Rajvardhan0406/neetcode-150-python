337fb28bf8cf15726810171a9c2a00e039b6bef9
Raj Vardhan Srivastava <vraj35415@gmail.com>
2026-08-01 20:52:37 +0530

    Time: 0 ms (100%), Space: 19.3 MB (51.96%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,16 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: list[str]) -> bool:
+        word_set = set(wordDict)
+        n = len(s)
+        max_len = max(len(w) for w in wordDict) 
+        
+        dp = [False] * (n + 1)
+        dp[0] = True  
+        
+        for i in range(1, n + 1):
+            for j in range(max(0, i - max_len), i):
+                if dp[j] and s[j:i] in word_set:
+                    dp[i] = True
+                    break
+        
+        return dp[n]
